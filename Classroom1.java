05dc0b961294715376619ad96c3d9d4ef1cd339f
ADITYASINGH-2001 <119028526+ADITYASINGH-2001@users.noreply.github.com>
2023-06-27 02:41:07 +0530

    Add files via upload

---
+++ Classroom1.java
@@ -0,0 +1,30 @@
+public class Classroom {
+	public static void changArr(int arr[],int i,int val)
+	{ 
+		if(i==arr.length)
+		{
+			printArr(arr);
+		    return;
+		    }
+		arr[i]=val;
+		changArr(arr,i+1,val+1);
+		arr[i]=arr[i]-2;
+		
+	}
+	public static void printArr(int arr[])
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int arr[]=new int[5];
+       changArr(arr,0,1);
+       printArr(arr);
+	}
+
+}
